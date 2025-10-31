9857e2cf14afb353e91db26a0382c1aeddf0d405
rioaristo13-cpu <rioaristo13@gmail.com>
2025-10-31 20:58:45 +0700

    Ubah KalkulatorSimple

--- Algoritma Pemrograman 1B/KalkulatorSimpel.py
+++ Algoritma Pemrograman 1B/KalkulatorSimpel.py
@@ -2,14 +2,12 @@ print("Masukan Angka 1")
 print("Masukan Angka 2")
 print("Dan Input Operator Aritmatika")
 print(30*"="+"\n")
+
 angka1 = float(input("Angka Pertama: "))
 angka2 = float(input("Angka Kedua: "))
 operasi = input("Masukkan Operasi (+,-,*,/): ")
-                                                                    #                                           x = 50
-                                                                    # 
-#Operasi perhitungan                  
-                              #!= itu untuk menegasi sebuah nilai, e.g if x != 50, False
-if operasi == "+":                                                  #                                        if x != 100, True
+
+if operasi == "+":
     hasil = angka1+angka2
     print(f"Hasil dari {angka1} + {angka2} = {hasil}")
 elif operasi == "-":
