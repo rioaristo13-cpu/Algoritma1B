53ef55e79399cd8823f30f77c384ddcb9c80ff38
rioaristo13-cpu <rioaristo13@gmail.com>
2025-11-21 10:37:03 +0700

    Penjelasan searching p-6 pap

--- Kode Python/Praktikum PAP1/P-6 20 November 2025/ACT Search/utamasearch.py
+++ Kode Python/Praktikum PAP1/P-6 20 November 2025/ACT Search/utamasearch.py
@@ -3,10 +3,13 @@ import searching
 data1 = [1,3,4,6,7,4,9,10,2,5,8,11]
 data2 = [1,7,8,9,10,11,13,16,22,23,24,56]
 
+print(f"Data 1 : {data1}")
+print(f"Data 2 : {data2}\n")
+
 index1 = searching.linear_search(data1,7)
 index2 = searching.binary_search(data2,23)
 
-print(f"Elemen target 7 ada di index ke-{index1}")
-print(f"Elemen target 7 ada di index ke-{index2}")
+print(f"Hasil linear search Data 1 : {index2}")
+print(f"Hasil binary search Data 2 : {index1}")
 
 
