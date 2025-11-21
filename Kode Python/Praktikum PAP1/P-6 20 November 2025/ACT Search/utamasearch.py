e2b4feda4bdbb17709451814f49b193e158ee5c2
rioaristo13-cpu <rioaristo13@gmail.com>
2025-11-21 10:32:39 +0700

    Format nama P-6

--- Kode Python/Praktikum PAP1/P-6 20 November 2025/ACT Search/utamasearch.py
+++ Kode Python/Praktikum PAP1/P-6 20 November 2025/ACT Search/utamasearch.py
@@ -4,11 +4,10 @@ import searching
 data1 = [1,3,4,6,7,4,9,10,2,5,8,11]
 data2 = [1,7,8,9,10,11,13,16,22,23,24,56]
 
-print(f"Jumlah data1 {len(data1)}")
-print(f"Jumlah data2 {len(data2)}\n")
+index1 = searching.linear_search(data1,7)
+index2 = searching.binary_search(data2,23)
 
-print(f"Elemen target 7 ada di index ke-{searching.linear_search(data1,7)}")
-
-print(f"Elemen target 7 ada di index ke-{searching.binary_search(data2,23)}")
+print(f"Elemen target 7 ada di index ke-{index1}")
+print(f"Elemen target 7 ada di index ke-{index2}")
 
 
--- Kode Python/Praktikum PAP1/P-6 20 November 2025/ACT Sort/utamasort.py
+++ Kode Python/Praktikum PAP1/P-6 20 November 2025/ACT Sort/utamasort.py
@@ -2,5 +2,10 @@ import sorting
 
 data = [5,1,2,5,7,3,10,9,12,11,8,4]
 
-print ("Selection Sorting : ", sorting.selection_sort(data))
-print ("Quick Sorting : ", sorting.quick_sort(data))
+print (f"Data : {data}\n")
+
+ssort = sorting.selection_sort(data)
+qsort = sorting.quick_sort(data)
+
+print (f"Hasil Selection Sorting : {qsort}")
+print (f"Hasil Quick Sorting : {ssort}")
