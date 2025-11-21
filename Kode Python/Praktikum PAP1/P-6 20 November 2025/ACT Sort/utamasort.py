d76834b2805f3159eb91f472cef02414c8e43a49
rioaristo13-cpu <rioaristo13@gmail.com>
2025-11-21 10:39:42 +0700

    betulin sort p-6 pap

--- Kode Python/Praktikum PAP1/P-6 20 November 2025/ACT Sort/utamasort.py
+++ Kode Python/Praktikum PAP1/P-6 20 November 2025/ACT Sort/utamasort.py
@@ -7,5 +7,5 @@ print (f"Data : {data}\n")
 ssort = sorting.selection_sort(data)
 qsort = sorting.quick_sort(data)
 
-print (f"Hasil Selection Sorting : {qsort}")
-print (f"Hasil Quick Sorting : {ssort}")
+print (f"Hasil Selection Sorting : {ssort}")
+print (f"Hasil Quick Sorting : {qsort}")
