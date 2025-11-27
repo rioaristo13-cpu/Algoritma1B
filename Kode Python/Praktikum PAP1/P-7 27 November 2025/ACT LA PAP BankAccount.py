3e96e2c1ffd48f095e1c464b1c2bee5f7af387c0
rioaristo13-cpu <rioaristo13@gmail.com>
2025-11-27 11:26:37 +0700

    ACT LA PAP

---
+++ Kode Python/Praktikum PAP1/P-7 27 November 2025/ACT LA PAP BankAccount.py
@@ -0,0 +1,18 @@
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.__balance = balance
+    def get_balance(self):
+        return self.__balance
+    def set_balance(self, amount):
+        if amount >= 0 :
+            self.__balance = amount
+        else:
+            print("Saldo tidak boleh negatif!")
+
+account = BankAccount("Aristo", 1000)
+print(f"Pemilik : {account.owner}")
+print(f"Balance : {account.get_balance()}")
+account.set_balance(1500)
+print(f"Balance Baru : {account.get_balance()}")
+account.set_balance(-500)  
