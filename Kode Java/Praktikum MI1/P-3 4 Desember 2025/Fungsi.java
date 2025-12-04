3fbc9b3a77d875bbf56c8023a7c0b62ce9ac4f46
rioaristo13-cpu <rioaristo13@gmail.com>
2025-12-04 23:48:09 +0700

    Ulangan pap p7 & ACT LA mi p3 4 desember 2025

---
+++ Kode Java/Praktikum MI1/P-3 4 Desember 2025/Fungsi.java
@@ -0,0 +1,146 @@
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class Fungsi {
+    
+    int[] HimpunanA;
+    int[] HimpunanB;
+    int[] HimpunanRelasi;
+    int[] HimpunanAsal;
+    int jmlHimpunanA, jmlHimpunanB, jmlRelasi;
+    Scanner input = new Scanner(System.in);
+
+        public static void main(String args[]) {
+            Fungsi madhun = new Fungsi();
+            madhun.inputAnggota();
+            madhun.cetakHimpunan();
+            madhun.inputRelasi();
+            madhun.cekFungsi();
+            madhun.cetakRelasi();
+
+        }
+
+        public void inputAnggota() {
+            out.print("Masukkan Jumlah Himpunan A = ");
+            jmlHimpunanA = input.nextInt();
+            
+            HimpunanA = new int[jmlHimpunanA];
+            for (int i = 0; i <= (jmlHimpunanA - 1); i++) {
+                out.print("Himpunan A yang ke " + (i+1) + " : ");
+                HimpunanA[i] = input.nextInt();
+            }
+
+            out.print("Masukkan Jumlah Himpunan B = ");
+            jmlHimpunanB = input.nextInt();
+            
+            HimpunanB = new int[jmlHimpunanB];
+            for (int i = 0; i <= (jmlHimpunanB - 1); i++) {
+                out.print("Himpunan B yang ke " + (i+1) + " : ");
+                HimpunanB[i] = input.nextInt();
+            }
+        }
+
+        public void cetakHimpunan() {
+            out.print ("Anggota Himpunan A = { ");
+            for (int i = 0; i <= (jmlHimpunanA - 1); i++) {
+                out.print(HimpunanA[i] + " ");
+            }
+            out.print("} ");
+
+            out.print ("Anggota Himpunan B = { ");
+            for (int i = 0; i <= (jmlHimpunanB - 1); i++) {
+                out.print(HimpunanB[i] + " ");
+            }
+            
+            out.print("} ");
+        }
+
+        public void inputRelasi() {
+            int jmlRelasiMaks = jmlHimpunanA * jmlHimpunanB;
+            out.println("Relasi Maksimal : "+jmlRelasiMaks);
+            out.print(" "); 
+            
+            do { 
+                out.print("Masukkan Jumlah Relasi : ");
+                jmlRelasi = input.nextInt();
+            } while (jmlRelasi > jmlRelasiMaks);
+
+            HimpunanRelasi = new int[jmlRelasi];
+            HimpunanAsal = new int[jmlRelasi];
+            out.println("Masukkan Relasinya : ");
+            for (int i = 0;i <= (jmlRelasi - 1); i++) {
+                int[] temp = new int[jmlRelasi];
+                int[] temp2 = new int[jmlRelasi];
+                boolean SamaA = false;
+                boolean SamaB = false;
+
+            out.println("Relasi ke" + (i+1) + " : ");
+            do { 
+                out.print("Masukkan Asal A :");
+                temp[i] = input.nextInt();
+                out.print("Masukkan Tujuan B : ");
+                temp2[i] = input.nextInt();
+
+                for(int j = 0; j <= (jmlHimpunanA - 1); j++) {
+                    if (temp2[i] == HimpunanA[j]) {
+                        SamaA = true;
+                    }
+                }
+                for(int k = 0; k <= (jmlHimpunanB - 1); k++) {
+                    if (temp2[i] == HimpunanB[k]) {
+                        SamaB = true;
+                    }
+                }
+                if (SamaA == false || SamaB == false) {
+                    out.println("Anggota Himpunan Tidak terdapat dikedua Himpunan A & B");
+                }
+                if (SamaB == true && SamaB == true) {
+                    HimpunanRelasi[i] = temp2[i];
+                    HimpunanAsal[i] = temp[i];
+                }
+            } while (SamaA == false || SamaB == false);
+        }
+    }
+    public void cekFungsi() {
+        int jumlahAnggota = 0;
+        boolean adaSama = false;
+        
+        for (int i = 0; i <= (jmlHimpunanA - 1); i++) {
+            for (int j = 0; j <= (jmlRelasi - 1); j++) {
+                if (HimpunanA[i] == HimpunanAsal[j]) {
+                    jumlahAnggota++;
+                }
+            }
+        }
+        if (jumlahAnggota == jmlHimpunanA && adaSama == false) {
+            out.println("Relasi yang diinput adalah Fungsi");
+        } else {
+            out.println("Relasi yang diinput merupakan relasi biasa " + jumlahAnggota +" "+ adaSama);
+        }
+    }
+    public void cetakRelasi() {
+        out.print("Daerah Domain = { ");
+        for (int i = 0; i <= jmlHimpunanA - 1; i++) {
+            out.print(HimpunanA[i] + " ");
+        }
+        out.println("}");
+
+        out.print("Daerah Kodomain = { ");
+        for (int i = 0; i <= jmlHimpunanB - 1; i++) {
+            out.print(HimpunanB[i] + " ");
+        }
+        out.println("}");
+
+        out.print("Range adalah = {");
+        for (int i = 0; i <= (jmlRelasi - 1); i++) {
+            boolean adaSama = false;
+            for (int j = i + 1; j <= (jmlRelasi - 1); j++) {
+                if (HimpunanRelasi[i] == HimpunanRelasi[j]);
+                    adaSama = true;
+            }
+            if (adaSama == false)
+                out.print(HimpunanRelasi[i]);
+        }
+        out.print("}");
+    }
+}
---
+++ Kode Python/Praktikum PAP1/Ulangan PAP1/ULANGAN PAP1.py
@@ -0,0 +1,71 @@
+while True:
+    def diskon():
+        nama =      input("Masukkan Nama Barang   : ")
+        harga = int(input("Masukkan Harga Barang  : "))
+        jumlah =int(input("Masukkan Jumlah Barang : "))
+        total = harga*jumlah
+        if total > 1000000:
+            tdiskon = total*0.5
+            dis = "50%"
+        elif total > 500000:
+            tdiskon = total*0.4
+            dis = "40%"
+        elif total > 100000:
+            tdiskon = total*0.2
+            dis = "20%"
+        else:
+            tdiskon = total
+            dis = "0%"
+        print (f"Nama Barang    : {nama}")
+        print (f"Harga Barang   : Rp {harga}")
+        print (f"Jumlah Barang  : {jumlah}")
+        print (f"Total Harga    : Rp {total}")
+        print (f"Harga Didiskon : Rp {tdiskon}")
+        print (f"Diskon Harga   : {dis}\n")
+
+    def fileuang():
+        with open ("file.txt","w") as file:
+            nama = input("Masukkan nama barang : ")
+            harga = input("Masukkan harga barang : ")
+            jumlah = input("Masukkan jumlah barang : ")
+            file.write(nama+"\n")
+            file.write(harga+"\n")
+            file.write(jumlah+"\n")
+        print ("")
+    def bacauang():
+        with open ("file.txt","r") as file:
+            for baris in (file):
+                print (baris)
+    def listbarang():
+        list = []
+        banyak = int(input("\nBanyak barang dalam gudang: "))
+        print ()
+
+        for i in range (banyak):
+            isi = input (f"Masukkan nama barang ke-{i+1}: ")
+            list.append(isi)
+        
+        print(list,"\n")
+
+    def main():
+        print ("PROGRAM PENGATURAN KEUANGAN")
+        print ("1. DISKON HARGA")
+        print ("2. FILE KEUANGAN")
+        print ("3. BACA FILE KEUANGAN")
+        print ("4. ISI GUDANG")
+        print ("5. KELUAR")
+        pil = int(input("Pilih Salah Satu Menu : "))
+        if pil == 1:
+            diskon()
+        elif pil == 2:
+            fileuang()
+        elif pil == 3:
+            bacauang()
+        elif pil == 4:
+            listbarang()
+        elif pil == 5:
+            print("Program Selesai...")
+            quit()
+        else:
+            print("Pilih yang ada di Menu.")
+    main()
